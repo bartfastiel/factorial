c50f711f2d39e16330e5fd91d3db0192250e90c2
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-05-24 11:31:01 +0200

    Calculate factorial with for loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("5! = " + factorial(5));
+    }
+
+    public static int factorial(int number) {
+        int result = 1;
+        for (int i = 1;i <= number;i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
